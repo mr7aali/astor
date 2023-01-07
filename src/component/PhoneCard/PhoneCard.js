e4f45de467b77871ce0d6b4e2b2a483186240d48
mr7aali <mr7aali@gmail.com>
2023-01-07 23:41:41 +0600

    added new card

--- src/component/PhoneCard/PhoneCard.js
+++ src/component/PhoneCard/PhoneCard.js
@@ -1,55 +1,169 @@
+import { Button, Divider } from '@mui/material';
 import React, { useContext } from 'react';
 import { AuthContext } from '../../contexts/AuthProvider';
+import TaskAltIcon from '@mui/icons-material/TaskAlt';
+import { styled } from '@mui/material/styles';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardMedia from '@mui/material/CardMedia';
+import CardContent from '@mui/material/CardContent';
+import CardActions from '@mui/material/CardActions';
+import Collapse from '@mui/material/Collapse';
+import Avatar from '@mui/material/Avatar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import { red } from '@mui/material/colors';
+import FavoriteIcon from '@mui/icons-material/Favorite';
+import ShareIcon from '@mui/icons-material/Share';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+
+import './PhoneCard.css';
+import { Box } from '@mui/system';
 
 
-import './PhoneCard.css'
+const ExpandMore = styled((props) => {
+    const { expand, ...other } = props;
+  
+    return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+    transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
+    marginLeft: 'auto',
+    transition: theme.transitions.create('transform', {
+        duration: theme.transitions.duration.shortest,
+    }),
+}));
+
+
+
 const PhoneCard = ({ phone }) => {
     
-   const tic = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-   <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
- </svg>
-    const {setModeal}=useContext(AuthContext);
-    const {   location, name,  postedTime, resalePrice, sellerName, yearsOfUse } = phone;
+    const [expanded, setExpanded] = React.useState(false);
+
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+    };
+
+
+    const tic = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+    </svg>
+    const { setModeal } = useContext(AuthContext);
+    const { location, name, postedTime, resalePrice, sellerName, yearsOfUse } = phone;
     return (
-        <div className='single-phone'>
-            <div className="card w-96 bg-base-100 shadow-xl">
-                <figure><img className='single-phone-img' src={phone?.picture} alt="Shoes" /></figure>
 
 
-                <div className="card-body">
+        
+
+        <Card sx={{ maxWidth: 345, position:'relative' }}>
+            <CardHeader
+                avatar={
+                    <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                        R
+                    </Avatar>
+                }
+                action={
+                    <IconButton aria-label="settings">
+                        <MoreVertIcon />
+                    </IconButton>
+                }
+                title={sellerName}
+              
+             
+                subheader={postedTime}
+            />
+            <CardMedia
+                component="img"
+                // height="194"
+                sx={{ height: '200px' }}
+                image={phone?.picture}
+                alt="Paella dish"
+            />
+            <CardContent>
+
+                <Box sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'space-between'
+                }}>
+                    <Typography gutterBottom variant="h5" component="div">
+                        {name}
+                    </Typography>
+                    <Typography gutterBottom variant="p" component="div">
+                        <span style={{ color: 'blue', fontWeight: '500' }}>{resalePrice}$</span>
+                    </Typography>
+                </Box>
 
 
-                    <h2 className="card-title text-primary text-2xl">{name}</h2>
-                    <p>Original Price: {phone.originalPrice}$</p>
-                    <p>Asking Price: {resalePrice}$</p>
-                    <p>Used: {yearsOfUse} y</p>
-                   
-                   <p>Seller: {sellerName } 
-                 { phone?.isVerified &&
-                   <small className='text-primary font-bold'>Verified</small> 
+               
+
+
+
+
+
+
+                <Box sx={{ display: 'grid' , gridTemplateColumns: '1fr 1fr' }}>
+                    <Box  ox sx={{borderRight:'1px solid #ADA5A2'}}>
+                        <Typography variant="body2" color="text.secondary">
+                        Asking Price: {resalePrice}$
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                        Used: {yearsOfUse}y
+                        </Typography>
+                        <Typography  variant="body2" color="text.secondary">
+                        Seller: {sellerName }  { phone?.isVerified &&
+                  <TaskAltIcon/> 
                    }
-                   
-                   </p>
-                 
-                   
-                    <p>Location: {location}</p>
-                    <p>Post On {postedTime}</p>
-
-
-
-
-                    <label  onClick={()=>setModeal(phone)} htmlFor="my-modal-3" className="btn btn-outline  w-full text.lg ">Book Now</label>
-
-                  
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
                      
+                        Location: {location}
+                        </Typography>
+                    </Box>
+                    <Box sx={{marginLeft:'5px'}} >
+                        <Typography variant="body2" color="text.secondary">
+                        Original Price: {phone.originalPrice}$
+                        </Typography>
+    
+                    </Box>
+                </Box>
 
-                </div>
+
+            </CardContent>
+            <CardActions sx={{ height:'50px'}} disableSpacing>
+
+
+
+               
 
 
 
 
-            </div>
-        </div>
+            </CardActions>
+
+            <Button
+                    variant="contained"
+                    color="error"
+                    fullWidth
+                    sx={{
+                        cursor: 'default',
+                        position:'absolute',
+                        bottom:'0px',
+                       
+
+
+                    }}
+                >
+                    <label
+                        style={{
+                            cursor: 'pointer',
+                            width: '100%'
+                        }}
+                        onClick={() => setModeal(phone)} htmlFor="my-modal-3" >Book Now</label>
+                </Button>
+
+        </Card>
+
     );
 };
 
--- src/component/Shared/Footer/Footer.js
+++ src/component/Shared/Footer/Footer.js
@@ -4,10 +4,12 @@ import { Link } from 'react-router-dom';
 const Footer = () => {
     return (
         <footer className="footer p-10 bg-neutral text-neutral-content mt-20 ">
+          
             <div>
             <img className='logo-img mb-10 mt-10' src="https://i.ibb.co/N3QTn2f/images.png" alt="" srcSet="" />
                 <p>ACME Industries Ltd.<br />Providing reliable tech since 1992</p>
             </div>
+
             <div>
                 <span className="footer-title mt-10">Social</span>
                 <div className="grid grid-flow-col gap-4 ">
@@ -16,6 +18,7 @@ const Footer = () => {
                     <Link href=' '><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></Link>
                 </div>
             </div>
+            
         </footer>
     );
 };
