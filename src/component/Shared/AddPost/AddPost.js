2b7b2bc0c8abd9573a601deb4cee6ce168b3fb9c
mr7aali <mr7aali@gmail.com>
2023-03-13 23:17:54 +0600

    add

--- src/component/Shared/AddPost/AddPost.js
+++ src/component/Shared/AddPost/AddPost.js
@@ -1,79 +1,93 @@
 import { Button, MenuItem, TextField } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import './AddPost.css'
 const AddPost = () => {
-    const Brands = [{ value: 'iPhone', label: 'iPhone'},{value: 'Samsung', label: 'Samsung' },{value: 'Xiaomi', label: 'Xiaomi' } ,{value: 'Other', label: 'Other' } ];
-    const condition = [  { value: 'Excellent', label: 'Excellent', },{ value: 'Good', label: 'Good', },{ value: 'Fair', label: 'Fair' }];
+    const Brands = [{ value: 'iPhone', label: 'iPhone' }, { value: 'Samsung', label: 'Samsung' }, { value: 'Xiaomi', label: 'Xiaomi' }, { value: 'Other', label: 'Other' }];
+    const condition = [{ value: 'Excellent', label: 'Excellent', }, { value: 'Good', label: 'Good', }, { value: 'Fair', label: 'Fair' }];
     const { register, reset, handleSubmit } = useForm();
+    const [File, setFile] = useState(null);
+    const getFile = (e) => {
+        setFile(URL.createObjectURL(e.target.files[0]));
+    }
     const handleSummition = data => {
         console.log(data);
-        const final_data = { ...data, Email: "admin@gmail.com" }
-        fetch('https://resolute-ai-software-server.vercel.app/post', {
-            method: 'POST',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(final_data)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                reset();
-            })
-
-
     }
     return (
         //add-student-container
-        <div className='max-w-screen-xl	mx-auto grid  pt-20 pb-24'>
-            <div className='add-student-header'>
-                <h5>Add Student</h5>
-                <h4>2 Feb 2023</h4>
+        <div style={{ border: '1px solid red' }} className='max-w-screen-xl grid grid-cols-2 mx-auto px-4  pt-20 pb-24'>
+
+
+            <div>
 
             </div>
-            <form onSubmit={handleSubmit(handleSummition)}>
-                <div className='input-container'>
-                    <div className='first-input-container'>
-                        <TextField  {...register("first_name")} id="outlined-basic" label="Name" variant="outlined" />
-                        <TextField {...register("middle_name")} id="outlined-basic" label="Resale Price" variant="outlined" />
-                        <TextField {...register("last_name")} id="outlined-basic" label="Orginal Price" variant="outlined" />
 
+            <div>
+                <div className='add-student-header'>
+                    <h5>Add Student</h5>
+                    <h4>2 Feb 2023</h4>
 
-
-                        <TextField {...register("Class")} id="outlined-select-currency" select label="Brand"  >
-                            {Brands.map((option) => (
-                                <MenuItem key={option.value} value={option.value}>
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-                        <TextField {...register("Division")} id="outlined-select-currency" select label="Select Division" >
-                            {condition.map((option) => (
-                                <MenuItem key={option.value} value={option.value}>
-                                    {option.label}
-                                </MenuItem>
-                            ))}
-                        </TextField>
-
-                        <TextField {...register("Roll")} id="outlined-basic" label="Mobile Number" variant="outlined" />
-                    </div>
-
-                    <div className='second-input-container'>
-                        <div className='address-input-container'>
-                            <TextField {...register("address1")} id="outlined-basic" label="Address Line 1" variant="outlined" />
-                            <input type="file" />
-                        </div>
-                        <div style={{ marginTop: '15px' }} className='first-input-container'>
-                            <TextField {...register("Landmark")} id="outlined-basic" label="Landmark" variant="outlined" />
-                            <TextField {...register("City")} id="outlined-basic" label="City" variant="outlined" />
-                            <TextField {...register("Pincode")} id="outlined-basic" label="Pincode" variant="outlined" />
-                            <Button type='submit'  className='from-button'>Add Student</Button>
-                        </div>
-                    </div>
                 </div>
-            </form>
+                <form onSubmit={handleSubmit(handleSummition)}>
+                    <div className='input-container'>
+                        <div className='first-input-container'>
+                            <TextField {...register('brandName')} id="outlined-basic" label="Name" variant="outlined" />
+                            <TextField {...register('resalePrice')} id="outlined-basic" label="Resale Price" variant="outlined" />
+                            <TextField  {...register('originalPrice')}  id="outlined-basic" label="Orginal Price" variant="outlined" />
+                            <TextField {...register('mobileNumber')} id="outlined-basic" label="Mobile Number" variant="outlined" />
 
+
+
+
+                            <TextField {...register('Brand')}  id="outlined-select-currency" select label="Brand"  >
+                                {Brands.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+                            <TextField {...register('condition')} id="outlined-select-currency" select label="Condition" >
+                                {condition.map((option) => (
+                                    <MenuItem key={option.value} value={option.value}>
+                                        {option.label}
+                                    </MenuItem>
+                                ))}
+                            </TextField>
+
+                            
+                        </div>
+
+                        <div className=''>
+
+
+                            <div className='grid grid-cols-2 mt-5 gap-3'>
+                                <TextField {...register('location')} id="outlined-basic" label="Address" variant="outlined" />
+
+                                <div className='row-span-2 from-img-container'>
+                                    <input className='' type="file" id='uploadFile' onChange={getFile} accept='image/*' />
+
+                                    <label htmlFor='uploadFile'>
+                                        {
+                                            File ?
+                                                <img {...register('img')} className='' src={File} alt="" /> :
+                                                <img className='' src='https://i.ibb.co/n6zD9sg/images.png' alt="" />
+                                        }
+                                    </label>
+                                </div>
+                                <TextField className='' {...register('yearsOfUse')} id="outlined-basic" label="Used (y)" variant="outlined" />
+                            </div>
+
+                            <div style={{ marginTop: '10px' }} className='grid'>
+
+
+                                <Button type='submit' className='from-button'>Submit Post</Button>
+                            </div>
+
+
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
